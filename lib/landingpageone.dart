a8cc1b2d21c0320a193f6e72a3d95a6803dae76d
msaudali94 <f2019105048@umt.edu.pk>
2022-08-13 03:52:23 -0700

    2nd

--- lib/landingpageone.dart
+++ lib/landingpageone.dart
@@ -14,8 +14,24 @@ class _LandingPageOne extends  State<LandingPageOne> {
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: Image.asset(Assets.img2),
-
+        decoration: BoxDecoration(
+          image: new DecorationImage(
+            image: new ExactAssetImage(Assets.img2),
+            fit: BoxFit.cover,
+          ),        ),
+        child:Container(
+          padding: EdgeInsets.symmetric(horizontal:MediaQuery.of(context).size.width*0.1 ),
+          color: Color(0xFF3A1772).withOpacity(0.64),
+          child: Column(
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height*0.45,),
+              Text("Protect your loved ones",
+                style: TextStyle(color: Colors.white, fontSize: 30.0),),
+              Text("Help your loved ones stay safeand give yourself peace of mindwhenever they go",
+                style: TextStyle(color: Colors.white, fontSize: 16.0),)
+            ],
+          ),
+        )
 
       ),
 
