c62879e30fca5ca83235d7793a299c15c44af147
msaudali94 <f2019105048@umt.edu.pk>
2022-08-14 12:15:28 -0700

    15/08 12:15 am

--- lib/homeuser.dart
+++ lib/homeuser.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_safe/res/Assets.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:user_profile_avatar/user_profile_avatar.dart';
 
 class HomeUser extends StatefulWidget{
   @override
@@ -32,16 +33,41 @@ class _HomeUser extends  State<HomeUser> {
 
                 SizedBox(height: MediaQuery.of(context).size.height*0.1,),
 
+        Container(
+          height: 80,
+          width: 80,
+          decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.white,
+                width: 2,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(60))
+          ),
+          //child: Image
+
+        ),
+
+                SizedBox(height: MediaQuery.of(context).size.height*0.01,),
+
+                Text("Hi Amanda",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
 
 
 
+                Text("We are looking out for you ",
+                  style: TextStyle(
+                    color: Color(0xFFA2A0A0),
+                    fontSize: 17.0,
+                    fontWeight: FontWeight.w400,
+                  ),),
 
-                SizedBox(height: MediaQuery.of(context).size.height*0.04,),
-
-
-
-
-
+                SizedBox(height: MediaQuery.of(context).size.height*0.02,),
 
 
               ],
