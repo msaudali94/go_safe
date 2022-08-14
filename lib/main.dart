bb83e622f00b475c12e8ab4ed2192dcbea34bb2a
msaudali94 <f2019105048@umt.edu.pk>
2022-08-14 10:25:18 -0700

    14/08 10:25 pm

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'landingpageone.dart';
 import 'landingpagetwo.dart';
 import 'signin.dart';
-
+import 'signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title:"Go Safe",
-      home: SignIn()
+      home: SignUp()
       );
   }
 }
--- lib/signup.dart
+++ lib/signup.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:go_safe/res/Assets.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 
-class SignIn extends StatefulWidget{
+class SignUp extends StatefulWidget{
   @override
-  _SignIn createState() => _SignIn();
+  _SignUp createState() => _SignUp();
 }
 
-class _SignIn extends  State<SignIn> {
+class _SignUp extends  State<SignUp> {
 
   final List<String> items = [
     'User',
@@ -25,12 +25,12 @@ class _SignIn extends  State<SignIn> {
           width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
             image: new DecorationImage(
-              image: new ExactAssetImage(Assets.img3),
+              image: new ExactAssetImage(Assets.img4),
               fit: BoxFit.cover,
             ),        ),
           child:Container(
             padding: EdgeInsets.symmetric(horizontal:MediaQuery.of(context).size.width*0.1 ),
-            color: Colors.black.withOpacity(0.8),
+            color: Colors.black.withOpacity(0.6),
 
             child: Column(
 
@@ -49,31 +49,63 @@ class _SignIn extends  State<SignIn> {
 
                 SizedBox(height: MediaQuery.of(context).size.height*0.08,),
 
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  enabledBorder:OutlineInputBorder(
-                    borderSide: BorderSide(color:Colors.transparent),
-                    borderRadius: BorderRadius.circular(5.5),),
 
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color:Colors.transparent),
-                    borderRadius: BorderRadius.circular(5.5),),
+                TextFormField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    enabledBorder:OutlineInputBorder(
+                      borderSide: BorderSide(color:Colors.transparent),
+                      borderRadius: BorderRadius.circular(5.5),),
 
-                  prefixIcon: Icon(
-                    Icons.email,
-                    size:30,
-                    color: Color(0xFF2C141F).withOpacity(0.85),),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color:Colors.transparent),
+                      borderRadius: BorderRadius.circular(5.5),),
 
-                  hintText: "Email",
-                  hintStyle: TextStyle(
-                    color: Color(0xFFA2A0A0),
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w400,),
-                  filled: true,
-                  fillColor: Colors.white,
+                    prefixIcon: Icon(
+                      Icons.email,
+                      size:30,
+                      color: Color(0xFF2C141F).withOpacity(0.85),),
+
+                    hintText: "Username",
+                    hintStyle: TextStyle(
+                      color: Color(0xFFA2A0A0),
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w400,),
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
                 ),
-              ),
+
+
+
+                SizedBox(height: MediaQuery.of(context).size.height*0.04,),
+
+
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    enabledBorder:OutlineInputBorder(
+                      borderSide: BorderSide(color:Colors.transparent),
+                      borderRadius: BorderRadius.circular(5.5),),
+
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color:Colors.transparent),
+                      borderRadius: BorderRadius.circular(5.5),),
+
+                    prefixIcon: Icon(
+                      Icons.email,
+                      size:30,
+                      color: Color(0xFF2C141F).withOpacity(0.85),),
+
+                    hintText: "Email",
+                    hintStyle: TextStyle(
+                      color: Color(0xFFA2A0A0),
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w400,),
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                ),
 
 
 
@@ -117,7 +149,7 @@ class _SignIn extends  State<SignIn> {
                           Icons.supervisor_account_rounded,
                           size:30,
                           color: Color(0xFF2C141F).withOpacity(0.85),
-                          ),
+                        ),
 
                         SizedBox(
                           width: 9,
@@ -183,46 +215,37 @@ class _SignIn extends  State<SignIn> {
                   ),
                 ),
 
-                SizedBox(height: MediaQuery.of(context).size.height*0.02,),
+                SizedBox(height: MediaQuery.of(context).size.height*0.1,),
 
-                TextButton(onPressed: (){},
-                    child: Text("Forgot Password?",
+
+
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.redAccent, // Background color
+                    minimumSize: const Size(400, 60),
+
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0),
+                    ),
+                  ),
+                  onPressed: () {},
+
+                  child: Text("Sign up",
+                    textAlign: TextAlign.center,
                     style: TextStyle(
-                      color: Color(0xFFA2A0A0),
-                      fontSize: 17.0,
-                      fontWeight: FontWeight.w400,
-                    ),),),
-
-                SizedBox(height: MediaQuery.of(context).size.height*0.08,),
-
-
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.blueAccent, // Background color
-            minimumSize: const Size(400, 60),
-
-            shape: new RoundedRectangleBorder(
-              borderRadius: new BorderRadius.circular(30.0),
-            ),
-          ),
-          onPressed: () {},
-
-          child: Text("Sign in",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 28.0,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-        ),
+                      color: Colors.white,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
 
                 SizedBox(height: MediaQuery.of(context).size.height*0.02,),
 
 
                 Row( mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text("Dont have an account?",
+                    Text("Already have an account?",
                       style: TextStyle(
                         color: Color(0xFFA2A0A0),
                         fontSize: 17.0,
@@ -230,7 +253,7 @@ class _SignIn extends  State<SignIn> {
                       ),),
 
                     TextButton(onPressed: (){},
-                      child: Text("Create new one",
+                      child: Text("Sign in",
                         style: TextStyle(
                           color: Colors.blueAccent,
                           fontSize: 17.0,
