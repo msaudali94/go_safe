81c7f05b12a5997e036b6355c985c684b7f7c1b4
msaudali94 <f2019105048@umt.edu.pk>
2022-08-16 10:12:00 -0700

    16/08 10:10 pm

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title:"Go Safe",
-      home: BatteryStatus()
+      home: Emergency()
       );
   }
 }
